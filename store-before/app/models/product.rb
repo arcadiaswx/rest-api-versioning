5619b9b3163d9e5e5ecd5888eee922c30015cd24
Craig Booker <craig@semantikmedia.com>
2013-09-22 16:59:52 -0500

    Got basic api to work.

--- store-before/app/models/product.rb
+++ store-before/app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  attr_accessible :name, :price, :released_on, :category, :category_id
+  attr_accessible :name, :price, :released_at, :category, :category_id
 end
--- store-before/db/schema.rb
+++ store-before/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515210216) do
+ActiveRecord::Schema.define(:version => 20130917004202) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(:version => 20120515210216) do
     t.string   "name"
     t.integer  "category_id"
     t.decimal  "price",       :precision => 2, :scale => 8
-    t.date     "released_on"
+    t.datetime "released_at"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
   end
